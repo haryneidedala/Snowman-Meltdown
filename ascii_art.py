3374404a1b4e2776145b682f34038b8c2d00584b
Haryneide Dala <hfloriano@icloud.com>
2025-06-22 23:40:34 +0200

    Update ascii_art.py
    Update ascii_art.py with more detailed melting stages:

--- ascii_art.py
+++ ascii_art.py
@@ -1,6 +1,5 @@
-# ascii_art.py
 STAGES = [
-    # Stage 0: Full snowman
+    # Stage 0: Full snowman (0 mistakes)
     """
       ___  
      /___\\ 
@@ -8,22 +7,44 @@ STAGES = [
      ( : ) 
      ( : ) 
     """,
-    # Stage 1: Bottom part starts melting
+    # Stage 1: Slight melting (1 mistake)
+    """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     (   ) 
+    """,
+    # Stage 2: Body starts melting (2 mistakes)
     """
       ___  
      /___\\ 
      (o o) 
      ( : ) 
     """,
-    # Stage 2: Only the head remains
+    # Stage 3: Body melting (3 mistakes)
     """
       ___  
      /___\\ 
      (o o) 
+     (   ) 
     """,
-    # Stage 3: Snowman completely melted
+    # Stage 4: Head melting (4 mistakes)
     """
       ___  
      /___\\ 
+     ( o ) 
+    """,
+    # Stage 5: Severe melting (5 mistakes)
+    """
+      ___  
+     /   \\ 
+     ( o ) 
+    """,
+    # Stage 6: Completely melted (6 mistakes)
+    """
+     \\ /  
+      X   
+     / \\ 
     """
 ]
